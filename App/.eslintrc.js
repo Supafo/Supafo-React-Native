8382d612217c3c391c4f9190ac2401e4848c8cc2
ebubekirkarakurt <ebukar75@gmail.com>
2024-05-10 03:18:29 +0300

    eslint updated

--- App/.eslintrc.js
+++ App/.eslintrc.js
@@ -1,19 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-<<<<<<< HEAD
-<<<<<<< HEAD
   requireConfigFile:false,
-=======
-  requireConfigFile: false,
   rules: {
     "prettier/prettier": [
       "error",
       { endOfLine: "auto" }
     ],
   },
->>>>>>> account-tab
-=======
-  requireConfigFile: false,
->>>>>>> FavoriteScreen
+
 };
