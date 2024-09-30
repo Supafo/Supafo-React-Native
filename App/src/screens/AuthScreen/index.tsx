7f6d734fa9d4fa6561bb3109b52e7d8e5f200096
Salih kırlıoglu <skirlioglu155@gmail.com>
2024-10-01 02:46:34 +0300

    AuthScreen redesigned + excluding cases

--- App/src/screens/AuthScreen/index.tsx
+++ App/src/screens/AuthScreen/index.tsx
@@ -19,6 +19,8 @@ import ActionSheet, {ActionSheetRef, ScrollView} from 'react-native-actions-shee
 import More from '../../assets/images/more_icon.png';
 import ModalCloseGreen from '../../assets/images/bottombaricons/ModalCloseGreen.svg';
 import responsiveScale from '../../utils/responsiveScale';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 const {scale, moderateScale, verticalScale} = responsiveScale;
 
 function AuthScreen() {
@@ -45,7 +47,7 @@ function AuthScreen() {
         style={{
           height: verticalScale(1),
           backgroundColor: '#DADADA',
-          width: '90%',
+          width: wp('90%'),
           alignSelf: 'center',
           borderRadius: 1,
           marginTop: top,
@@ -146,21 +148,21 @@ function AuthScreen() {
             </View>
              <Divider top={moderateScale(10)} />
              <View style={{right: moderateScale(8.25)}}>
-            <View style={{width: '100%', paddingHorizontal: moderateScale(21), marginTop: moderateScale(10),}}>
-              <Text style={{fontSize: moderateScale(15), color: '#000000', fontWeight: '600'}}>
+            <View style={{width: wp('100%'), paddingHorizontal: moderateScale(21), marginTop: moderateScale(10),}}>
+              <Text style={{fontSize: moderateScale(14.5), color: '#000000', fontWeight: '500'}}>
                 Zorunlu Çerezler
               </Text>
               <View
                 style={{
-                  width: '100%',
+                  width: wp('88%'),
                   flexDirection: 'row',
                   alignItems: 'center',
                   justifyContent: 'space-between',
                 }}>
                 <Text
-                  style={{fontSize: moderateScale(12), color: '#000000', fontWeight: '600',marginTop:moderateScale(-1)}}>
+                  style={{fontSize: moderateScale(12), color: '#000000', fontWeight: '500',marginTop:moderateScale(-1)}}>
 
-                  Teknik olarak gerekli ve istatistik veriler
+                  Teknik olarak gerekli ve istatistiksel veriler
                 </Text>
                 <View style={{bottom:moderateScale(17.5), left: moderateScale(8.25)}}>
                 <Switch
@@ -188,7 +190,8 @@ function AuthScreen() {
                 />
                 </View>
               </View>
-              <Text
+              <View style={{width:wp('93%')}}>
+                <Text
                 style={{
                   fontSize: moderateScale(12.25),
                   color: 'rgba(0,0,0,0.5)',
@@ -206,12 +209,15 @@ function AuthScreen() {
                 istatistiksel veriler toplanır ve uygulamamızın performansını ve
                 kullanıcı deneyimini geliştirmek için kullanılır.
               </Text>
+              </View>
+              
               <Pressable
                 onPress={() => {
                   setCookiesSheetStatus(1);
                 }}
                 style={{
-                  marginTop: moderateScale(6),
+                  marginTop: verticalScale(7),
+                  marginBottom: verticalScale(4.5),
                   flexDirection: 'row',
                   alignItems: 'center',
                   display: 'flex',
@@ -223,16 +229,15 @@ function AuthScreen() {
                     lineHeight: moderateScale(18),
                     color: '#66AE7B',
                     opacity:0.8,
-                    fontWeight: '700',
-                    fontFamily: 'Inter',
+                    fontWeight: '500',
                   }}>
                   Devamını Oku
                 </Text>
                 <Image source={More} style={{height: moderateScale(10.5), width: moderateScale(7),}} />
               </Pressable>
             </View>
-            <View style={{width: '100%', paddingHorizontal: moderateScale(21), marginTop: moderateScale(20)}}>
-              <Text style={{fontSize: moderateScale(15), color: '#000000', fontWeight: '500'}}>
+            <View style={{width: wp('100%'), paddingHorizontal: moderateScale(21), marginTop: moderateScale(20)}}>
+              <Text style={{fontSize: moderateScale(14.5), color: '#000000', fontWeight: '500'}}>
                 İsteğe Bağlı Çerezler
               </Text>
               <View
@@ -243,7 +248,7 @@ function AuthScreen() {
                   justifyContent: 'space-between',
                 }}>
                 <Text
-                  style={{fontSize: moderateScale(12.5), color: '#000000', fontWeight: '500',top:moderateScale(-1)}}>
+                  style={{fontSize: moderateScale(12), color: '#000000', fontWeight: '500',top:moderateScale(-1)}}>
 
                   Pazarlama
                 </Text>
@@ -283,7 +288,6 @@ function AuthScreen() {
                   marginTop: moderateScale(10),
                   position:'relative',
                   overflow:'scroll',
-                  //height: verticalScale(82.5),
                 }}>
                 Kişisel verilerinizi, size ilgi alanlarınıza uygun
                 kişileştirilmiş reklamlar ve içerik gösterebilmek amacıyla
@@ -311,11 +315,11 @@ function AuthScreen() {
                 }}>
                 <Text
                   style={{
+                    marginTop:verticalScale(2),
                     fontSize: moderateScale(13),
                     lineHeight: moderateScale(18),
                     color: '#66AE7B',
-                    fontWeight: '700',
-                    fontFamily: 'Inter',
+                    fontWeight: '500',
                   }}>
                   Devamını Oku
                 </Text>
@@ -327,14 +331,15 @@ function AuthScreen() {
               style={{
                 alignItems: 'center',
                 justifyContent: 'center',
-                width: '100%',
+                width: wp('100%'),
               }}></View>
             <Button
               style={{
                 borderRadius: moderateScale(15),
-                width: '90%',
+                width: wp('90%'),
+                left:moderateScale(7),
                 alignSelf: 'center',
-                height: moderateScale(40),
+                height: hp('5.25%'),
                 marginTop: moderateScale(20),
                 position:'relative',
                 zIndex:999,
@@ -348,9 +353,10 @@ function AuthScreen() {
             <Button
               style={{
                 borderRadius: moderateScale(15),
-                width: '90%',
+                width: wp('90%'),
+                left:moderateScale(7),
                 alignSelf: 'center',
-                height: moderateScale(40),
+                height: hp('5.25%'),
                 marginTop: moderateScale(10),
                 position:'relative',
                 zIndex:999,
@@ -373,9 +379,9 @@ function AuthScreen() {
         return (
           <View
             style={{
-              height: '80%',
+              height: hp('80%'),
               marginTop: moderateScale(24),
-              width: '100%',
+              width: wp('100%'),
               paddingHorizontal: moderateScale(21),
               alignItems: 'center',
             }}>
@@ -521,10 +527,13 @@ function AuthScreen() {
 
   return (
     <View style={styles.main}>
-      <Image source={Icon} style={styles.imageStyle } />
+      <View style={styles.imageContainer}>
+        <Image source={Icon} style={styles.imageStyle } />
+      </View>
+      
       <View style={styles.btnContainer}>
         <Button
-          style={{borderRadius: moderateScale(15), width:'112.5%', height:moderateScale(40),justifyContent:'center'}}
+          style={{borderRadius: moderateScale(15), width:wp('89%'), height:hp('5.25%'),justifyContent:'center'}}
           onPress={() => {
             if (isFirstSelected && isSecondSelected) {
               showCookiesSheet();
@@ -536,7 +545,7 @@ function AuthScreen() {
           Giriş Yap
         </Button>
         <Button
-          style={{borderRadius: moderateScale(15), width:'112.5%', height:moderateScale(40),justifyContent:'center'}}
+          style={{borderRadius: moderateScale(15), width:wp('89%'), height:hp('5.25'),justifyContent:'center'}}
           onPress={() => {
             if (isFirstSelected === true && isSecondSelected === true) {
               setLoginOrSignin('signup');
@@ -585,7 +594,7 @@ function AuthScreen() {
           </View>
 
           <View style={{width: '100%',}}>
-            <Text style={{fontSize: moderateScale(14), color: '#000000',right: moderateScale(7.75)}}>
+            <Text style={{fontSize: moderateScale(14), color: '#000000',right: moderateScale(7.75),fontWeight:'300'}}>
               Supafo’nun e-posta adresimi ve adımı gizlilik politikasına uygun
               şekilde saklamasına izin veriyorum.
             </Text>
@@ -629,9 +638,9 @@ function AuthScreen() {
             <Text style={[styles.policies, {marginRight: moderateScale(10)}]}>
               Şartlar & Koşulları
             </Text>
-            <Text style={{fontSize: moderateScale(14)}}> ve </Text>
+            <Text style={{fontSize: moderateScale(14),fontWeight:'300'}}> ve </Text>
             <Text style={styles.policies}>Gizlilik Politikasını</Text>
-            <Text> kabul ediyorum.</Text>
+            <Text style={{fontWeight:'300'}}> kabul ediyorum.</Text>
           </Text>
         </View>
       </View>
@@ -674,20 +683,19 @@ function AuthScreen() {
           }}>
           <Text
             style={{
-              fontWeight: '500',
+              fontWeight: '400',
               color: '#000000',
-              fontSize: moderateScale(15),
+              fontSize: moderateScale(14),
               marginTop: moderateScale(5),
             }}>
             Şartlar ve Gizlilik Onayı
           </Text>
-          <View style={{width: '81%'}}>
+          <View style={{width: wp('81%')}}>
             <Text
               style={{
                 marginTop: moderateScale(15),
                 color: '#000000',
-                fontFamily: 'Inter',
-                fontWeight: '400',
+                fontWeight: '300',
                 fontSize: moderateScale(12),
                 textAlign:'center',
               }}>
@@ -700,10 +708,11 @@ function AuthScreen() {
         <Button
           style={{
             borderRadius: moderateScale(15),
-            width: '81%',
+            width: wp('88.75%'),
+            height: hp('5.25%'),
             alignSelf: 'center',
             marginTop: moderateScale(27.5),
-            marginBottom: moderateScale(-17.5)
+            marginBottom: moderateScale(-18.5)
           }}
           onPress={() => {
             actionSheetRef.current?.hide();
@@ -732,17 +741,22 @@ const styles = StyleSheet.create({
   },
   imageStyle:{
     resizeMode:"contain",
-    height:moderateScale(250),
-    top: moderateScale(70),
+    height:hp('33%'),
+  },
+  imageContainer: {
+    flex:2.88,
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop: verticalScale(15),
   },
   btnContainer: {
     alignItems: 'center',
-    justifyContent: 'center',
-    width: '90%',
+    justifyContent: 'flex-end',
+    width: wp('89%'),
     display: 'flex',
     gap: moderateScale(11),
     marginHorizontal: moderateScale(20),
-    top: moderateScale(107.5),
+    flex:0.58,
   },
   checkboxes: {
     display: 'flex',
@@ -750,7 +764,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: moderateScale(35),
     borderColor: 'black',
     width: '100%',
-    top: moderateScale(65),
+    flex:0.775,
+    justifyContent:'flex-end',
+    marginBottom: verticalScale(7.5),
   },
   banner: {
     width: '95%',
@@ -759,6 +775,7 @@ const styles = StyleSheet.create({
     textDecorationStyle: 'solid',
     textDecorationLine: 'underline',
     color: '#66AE7B',
+    fontWeight:'300',
     marginLeft: moderateScale(10),
     marginRight: moderateScale(10),
   },
